cba9b8458123c0a0de2a45289e6077d8236091f7
lebrancconvas <lebrancconvas@gmail.com>
2021-12-28 17:11:19 +0700

    💄 Add New Calculator on Homepage UI.

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -6,6 +6,7 @@ import {Box} from '@mui/system';
 const Homepage = () => {
 	const navigator = useNavigate(); 
 	const toGCDLCM = () => navigator('/gcdlcm'); 
+	const toIsPrime = () => navigator('/isprime'); 
 	return(
 		<div>
 			<Box sx={{textAlign: 'center'}}> 
@@ -14,8 +15,14 @@ const Homepage = () => {
 			<Box sx={{textAlign: 'center'}} mt={3}> 
 				<Typography variant="h5">Select Calculator that you want.</Typography> 
 			</Box>
-			<Box sx={{textAlign: 'center'}} mt={2} onClick={toGCDLCM}>   
-				<Button variant="contained">GCD-LCM Calculator</Button> 
+			<Box sx={{textAlign: 'center'}} mt={2}>   
+				<Button variant="contained" onClick={toGCDLCM}>GCD-LCM Calculator</Button> 
+			</Box>
+			<Box sx={{textAlign: 'center'}} mt={2}>   
+				<Button variant="contained" onClick={toIsPrime}>Is this number is a prime number?</Button> 
+			</Box>
+			<Box sx={{textAlign: 'center'}} mt={2}> 
+				<Button variant="contained">Trigonometric Calculator</Button>  
 			</Box>
 		</div>
 	)
