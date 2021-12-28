d7f4936765b64f6c1f5e1a73bf9648cf3762eed4
lebrancconvas <lebrancconvas@gmail.com>
2021-12-28 15:06:32 +0700

    💄 Create Homepage UI.

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom'; 
+import {Typography, Button} from '@mui/material'; 
+import {Box} from '@mui/system'; 
 
 const Homepage = () => {
+	const navigator = useNavigate(); 
+	const toGCDLCM = () => navigator('/gcdlcm'); 
 	return(
-		<div></div>
+		<div>
+			<Box sx={{textAlign: 'center'}}> 
+				<Typography variant="h2">Mathematical Calculator</Typography> 
+			</Box>
+			<Box sx={{textAlign: 'center'}} mt={3}> 
+				<Typography variant="h5">Select Calculator that you want.</Typography> 
+			</Box>
+			<Box sx={{textAlign: 'center'}} mt={2} onClick={toGCDLCM}>   
+				<Button variant="contained">GCD-LCM Calculator</Button> 
+			</Box>
+		</div>
 	)
 }
 
