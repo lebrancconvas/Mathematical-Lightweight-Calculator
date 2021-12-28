b07d39bf7ae7b4a94e70aef446afc55dda3141ff
lebrancconvas <lebrancconvas@gmail.com>
2021-12-28 17:10:44 +0700

    👔 Add Router.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
 import Homepage from './pages/Homepage'; 
 import GCDLCM from './pages/calculator/GCDLCM'; 
+import IsPrime from './pages/calculator/IsPrime'; 
 function App() {
   return (
     <div>
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Homepage />} /> 
           <Route path="/gcdlcm" element={<GCDLCM />} />
+          <Route path="/isprime" element={<IsPrime />} /> 
         </Routes>
       </Router>
     </div>
