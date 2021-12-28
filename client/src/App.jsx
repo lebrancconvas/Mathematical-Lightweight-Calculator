2105727fc8d0557c31583a274c99a433fc592107
lebrancconvas <lebrancconvas@gmail.com>
2021-12-28 15:06:05 +0700

    👔 Add Router.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
+import Homepage from './pages/Homepage'; 
+import GCDLCM from './pages/calculator/GCDLCM'; 
 function App() {
   return (
     <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} /> 
+          <Route path="/gcdlcm" element={<GCDLCM />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
